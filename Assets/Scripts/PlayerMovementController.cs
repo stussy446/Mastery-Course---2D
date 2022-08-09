eb785d4ffa7ed28fdc6c4094846db0a378ba1988
steve rector <steve.rector@datadoghq.com>
2022-08-08 17:15:47 -0700

    Adds jump with rigidbody2d
    yay

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -3,15 +3,27 @@ using UnityEngine;
 public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float jumpForce = 400f;
+    Rigidbody2D rb2d;
 
-    // Update is called once per frame
-    private void Update()
+    private void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
 
 
-        Vector3 movement = new Vector3(horizontal, vertical, 0);
+        Vector3 movement = new Vector3(horizontal, 0, 0);
         transform.position += movement * Time.deltaTime * moveSpeed;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            rb2d.AddForce(Vector2.up * jumpForce);
+        }
     }
+
 }
