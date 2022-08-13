11ca0b1134b3883597509974d719dabbe36f23f4
steve rector <steve.rector@datadoghq.com>
2022-08-12 23:24:06 -0700

    character can only jump in the air

---
+++ Assets/Scripts/CharacterGrounding.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterGrounding : MonoBehaviour
+{
+
+    [SerializeField] private Transform leftFoot;
+    [SerializeField] private Transform rightFoot;
+    [SerializeField] private float maxDistance;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private bool isGrounded;
+
+
+    public bool IsGrounded { get => isGrounded; }
+      
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+        CheckFootForGrounding(leftFoot);
+        if (isGrounded == false)
+        {
+            CheckFootForGrounding(rightFoot);
+        }   
+
+
+
+    }
+
+
+    private void CheckFootForGrounding(Transform foot)
+    {
+        RaycastHit2D rayCastHit = Physics2D.Raycast(foot.position, Vector2.down, maxDistance, layerMask);
+        Debug.DrawRay(foot.position, Vector3.down * maxDistance, Color.red);
+
+        if (rayCastHit.collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+    }
+}
--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -5,10 +5,13 @@ public class PlayerMovementController : MonoBehaviour
     [SerializeField] private float moveSpeed = 2f;
     [SerializeField] private float jumpForce = 400f;
     Rigidbody2D rb2d;
+    CharacterGrounding characterGrounding;
 
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        characterGrounding = GetComponent<CharacterGrounding>();
+
     }
 
 
@@ -20,7 +23,7 @@ public class PlayerMovementController : MonoBehaviour
         Vector3 movement = new Vector3(horizontal, 0, 0);
         transform.position += movement * Time.deltaTime * moveSpeed;
 
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && characterGrounding.IsGrounded == true)
         {
             rb2d.AddForce(Vector2.up * jumpForce);
         }
