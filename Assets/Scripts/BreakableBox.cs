c3c5cc635f2f4e6c297834e4af8f34c8ce96a6fe
steve rector <steve.rector@datadoghq.com>
2022-08-18 00:22:39 -0700

    Adds background music and breakable box
    right now breakable box on destroys the game object

---
+++ Assets/Scripts/BreakableBox.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class BreakableBox : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (WasHitByPlayer(collision) && WasHitFromBottomSide(collision))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private static bool WasHitByPlayer(Collision2D collision)
+    {
+        return collision.collider.GetComponent<PlayerMovementController>() != null;
+    }
+
+    private static bool WasHitFromBottomSide(Collision2D collision)
+    {
+        return collision.contacts[0].normal.y > 0.5;
+    }
+
+}
