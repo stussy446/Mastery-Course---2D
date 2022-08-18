cc71d3933878783b567a77ee759528ffaf66dac0
steve rector <steve.rector@datadoghq.com>
2022-08-18 01:03:48 -0700

    Adds Spinning Saw Blade that kills player on touch

--- Assets/Scripts/Saw.cs
+++ Assets/Scripts/Saw.cs
@@ -2,22 +2,37 @@ using UnityEngine;
 
 public class Saw : MonoBehaviour
 {
-
     [SerializeField] Transform start;
     [SerializeField] Transform end;
     [SerializeField] Transform sawSprite;
+    [SerializeField] float sawSpeed = 3f;
+
     private float positionPercent;
     private int direction = 1;
 
     void Update()
     {
-        positionPercent += Time.deltaTime * direction;
+        MoveSaw();
+        SwitchSawDirectionIfNeeded();
+    }
+
+    private void MoveSaw()
+    {
+        float distance = Vector3.Distance(start.position, end.position);
+        float speedForDistance = sawSpeed / distance;
+
+        positionPercent += Time.deltaTime * direction * speedForDistance;
+
         sawSprite.position = Vector3.Lerp(start.position, end.position, positionPercent);
+    }
+
+    private void SwitchSawDirectionIfNeeded()
+    {
         if (positionPercent >= 1 && direction == 1)
         {
             direction = -1;
         }
-        else if(positionPercent <=0 && direction == -1)
+        else if (positionPercent <= 0 && direction == -1)
         {
             direction = 1;
         }
