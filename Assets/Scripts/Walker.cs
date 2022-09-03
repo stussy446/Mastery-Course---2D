5ac40b038b99b2cdb478422600b5d4afc59decfb
steve rector <steve.rector@datadoghq.com>
2022-09-02 23:14:18 -0700

    Adds ability for snail to flip when reaching edge

--- Assets/Scripts/Walker.cs
+++ Assets/Scripts/Walker.cs
@@ -7,17 +7,63 @@ public class Walker : MonoBehaviour
 
     private new Collider2D collider;
     private new Rigidbody2D rigidbody2D;
+    private SpriteRenderer spriteRenderer;
+
     private Vector2 direction = Vector2.left;
 
     private void Awake()
     {
         collider = GetComponent<Collider2D>();
         rigidbody2D = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
         rigidbody2D.MovePosition(rigidbody2D.position + direction * speed * Time.deltaTime);
+    }
+
+    private void LateUpdate()
+    {
+        if (ReachedEdge())
+        {
+            FlipSprite();
+            SwitchDirections();
+        }
+    }
+
+    private bool ReachedEdge()
+    {
+        float x = direction.x == -1 ?
+            collider.bounds.min.x - 0.1f :
+            collider.bounds.max.x + 0.1f;
+
+        float y = collider.bounds.min.y;
+
+        Vector2 origin = new Vector2(x, y);
+
+        Debug.DrawRay(origin, Vector3.down * 0.1f);
+
+        var hit = Physics2D.Raycast(origin, Vector2.down, 0.1f);
+        if (hit.collider == null)
+        {
+            return true;
+            
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void FlipSprite()
+    {
+        spriteRenderer.flipX = !spriteRenderer.flipX;
+    }
+
+    private void SwitchDirections()
+    {
+        direction *= -1;
     }
 
 }
